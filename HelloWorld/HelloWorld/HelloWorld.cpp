d58a061ac410a0715a99539611b9fd230b523a42
lzxsz <davylin@163.com>
2021-08-14 12:51:21 +0800

    Hello World
    This is a Hello World example.

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,27 @@
+// HelloWorld.cpp : 定义控制台应用程序的入口点。
+//
+
+/*
+ *	This is VS2010 Create Code.
+ *
+ *	#include "stdafx.h"
+ *
+ *
+ *	int _tmain(int argc, _TCHAR* argv[])
+ *	{
+ *		return 0;
+ *	}
+ *
+*/
+
+//This is my modified code.
+#include <stdio.h>   //For printf()
+#include <stdlib.h>  //For system()
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+
+	system("pause");  //For pause, any Key continue.
+	return 0;
+}
