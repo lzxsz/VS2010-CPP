d2f8082607260a6607c04d9180e0b0cc9dd2794d
lzxsz <davylin@163.com>
2021-09-03 04:38:15 +0800

    Hello POSIX Threads
    This is a POSIX Threads For Win32 example.

---
+++ Hello_POSIX_Threads/Hello_POSIX_Threads/Hello_POSIX_Threads.cpp
@@ -0,0 +1,13 @@
+// Hello_POSIX_Threads.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//
+
+//To See: hello-pthread.c
+
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
