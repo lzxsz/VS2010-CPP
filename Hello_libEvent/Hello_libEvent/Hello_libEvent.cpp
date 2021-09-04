cc2018c6875b4330dd1b06e73bb4d189fb3f0b83
lzxsz <davylin@163.com>
2021-09-05 00:28:50 +0800

    Hello ibEvent
    This is a libEvent Hello World example.

---
+++ Hello_libEvent/Hello_libEvent/Hello_libEvent.cpp
@@ -0,0 +1,12 @@
+﻿// Hello_libEvent.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//To See: helloworld_libevent.c
+
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
